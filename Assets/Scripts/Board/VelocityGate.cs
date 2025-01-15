705cb298d2dc2e0a8133db3b96fdc3e4ecdc97fc
Max <61809127+maxwellpark@users.noreply.github.com>
2025-01-15 14:55:22 +0000

    Fix velocity gate

--- Assets/Scripts/Board/VelocityGate.cs
+++ Assets/Scripts/Board/VelocityGate.cs
@@ -5,8 +5,11 @@ public class VelocityGate : MonoBehaviour
     [SerializeField] private float velocityThreshold = 10f;
     [SerializeField] private Collider2D gateCollider;
 
+    private SpriteRenderer spriteRenderer;
+
     private void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         gateCollider.isTrigger = true;
     }
 
@@ -16,16 +19,24 @@ public class VelocityGate : MonoBehaviour
         {
             var ballRb = collision.GetComponent<Rigidbody2D>();
             var velocity = ballRb.velocity.magnitude;
+            //var contactPoint = collider.GetContacts(0).point;
+            //var pointVelocity = ballRb.GetPointVelocity(contactPoint).magnitude;
 
             if (velocity >= velocityThreshold)
             {
                 gateCollider.isTrigger = true;
                 Debug.Log($"[gate] Ball entered. Velocity: {velocity}");
+
+                var color = spriteRenderer.color;
+                spriteRenderer.color = new Color(color.r, color.g, color.b, 0.25f);
             }
             else
             {
                 gateCollider.isTrigger = false;
                 Debug.Log($"[gate] Ball blocked. Velocity: {velocity}");
+
+                // Ensure we reset the trigger after blocking the ball as we might not get an OnTriggerExit2D message 
+                Invoke(nameof(OnBlocked), 0.5f);
             }
         }
     }
@@ -36,6 +47,15 @@ public class VelocityGate : MonoBehaviour
         {
             Debug.Log("[gate] Ball left");
             gateCollider.isTrigger = true;
+
+            var color = spriteRenderer.color;
+            spriteRenderer.color = new Color(color.r, color.g, color.b, 1f);
         }
+    }
+
+    private void OnBlocked()
+    {
+        Debug.Log("[gate] OnBlocked invoked");
+        gateCollider.isTrigger = true;
     }
 }
