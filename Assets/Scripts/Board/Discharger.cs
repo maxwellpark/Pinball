e80e8c42efda573a72998d5fa8984098fbd8177d
Max <61809127+maxwellpark@users.noreply.github.com>
2025-05-12 20:43:34 +0100

    Add lock guards to remaining receiver child classes

--- Assets/Scripts/Board/Discharger.cs
+++ Assets/Scripts/Board/Discharger.cs
@@ -8,7 +8,7 @@ public class Discharger : ReceiverBase
 
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.IsBall() || !collision.TryGetComponent<Ball>(out var ball) || !ball.IsCharged)
+        if (IsLocked || !collision.IsBall() || !collision.TryGetComponent<Ball>(out var ball) || !ball.IsCharged)
         {
             return;
         }
